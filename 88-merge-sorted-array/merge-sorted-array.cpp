97f4130239381ea22cb7503a32f7c6cff9e960f3
Shreyy <shreyw134@gmail.com>
2025-01-14 21:11:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (77.63%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,7 +1,26 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        //Optimal 1 by comparing the largest and smallest of both arrays and sorting all
+        //TC - min(n,m)+nlogn+mlogm , SC-O(1)
+        int left=m-1;
+        int right=0;
+        while(left>=0 && right<n){
+            if(nums1[left]>nums2[right]){
+                swap(nums1[left],nums2[right]);
+                left--;
+                right++;
+            }else{
+                break;
+            }
+        }
+        sort(nums1.begin(),nums1.begin()+m);
+        sort(nums2.begin(),nums2.end());
+        for(int i=m;i<n+m;i++){
+            nums1[i]=nums2[i-m];
+        }
         //Brute Force by Merge func of merge sort TC-n+m SC=n+m
+        /*
         vector<int>arr3(n+m,0);
         int left=0;
         int right=0;
@@ -30,5 +49,6 @@ public:
         for(int i=0;i<m+n;i++){
             nums1[i]=arr3[i];
         }
+        */
     }
 };
