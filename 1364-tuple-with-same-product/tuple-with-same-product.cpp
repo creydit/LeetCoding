5ac5d69f6e39a60221514f505ded195d5439806e
Shreyy <shreyw134@gmail.com>
2025-02-07 12:17:13 +0530

    Time: 340 ms (34.06%) | Memory: 84.8 MB (86.38%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -3,7 +3,7 @@ public:
     int tupleSameProduct(vector<int>& nums) {
         //FTS By me, POTD
         int ans=0;
-        map<int,int>mpp;
+        unordered_map<int,int>mpp;
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
                 int p=nums[i]*nums[j];
