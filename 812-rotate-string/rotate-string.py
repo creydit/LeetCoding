ed660e7c6559d969b7cf9fa1b75b03d5837d01dd
Shreyy <shreyw134@gmail.com>
2025-06-21 13:43:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (96.24%) - LeetSync

--- 812-rotate-string/rotate-string.py
+++ 812-rotate-string/rotate-string.py
@@ -1,11 +1,19 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        #Brute force by checking all rotation to goal TC-O(n2) and SC-O(n)
+        #Better by New Approach that doubling the string S to get every possible rotation of it as a substring!!
+        #TC- O(n) SC-O(n)
         if len(s)!=len(goal):
-            return False
-        n=len(s)
-        for _ in range(n):
-            s=s[1:]+s[0]
-            if s==goal:
-                return True
-        return False
+             return False
+        ds=s+s
+        return ds.find(goal)!=-1
+
+
+        #Brute force by checking all rotation to goal TC-O(n2) and SC-O(n)
+        # if len(s)!=len(goal):
+        #     return False
+        # n=len(s)
+        # for _ in range(n):
+        #     s=s[1:]+s[0]
+        #     if s==goal:
+        #         return True
+        # return False
