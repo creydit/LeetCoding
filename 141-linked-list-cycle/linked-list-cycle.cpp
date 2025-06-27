39138a39e8b3bc46005dd2db72ce9401783a87e9
Shreyy <shreyw134@gmail.com>
2025-06-27 23:42:23 +0530

    Time: 12 ms (39.96%) | Memory: 11.8 MB (52.98%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,9 +9,23 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        //Optimal by using a Tortoise and Hare Algo of using 2 pointer one slow (moves 1 step) 
+        //and other fast (moves 2 steps)
+        //TC ~ O(n) and SC- O(1)
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) return true;
+        }
+        return false;
+
+        
         //Brute Force thinking of using a map to store the nodes and mark them as visited or not 
         //and if a already checked nodes comes again we find a loop
         //TC - O(n * 2 * logn) and SC - O(n) as map is used 
+        /*
         unordered_map<ListNode* , int> mpp;
         ListNode* temp = head;
         while(temp){
@@ -22,5 +36,6 @@ public:
             temp = temp->next;
         }
         return false;
+        */
     }
 };
