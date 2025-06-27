77ea1e95845ec5f87aeff3186fefec3cf20f992b
Shreyy <shreyw134@gmail.com>
2025-06-27 23:00:10 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (18.96%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -11,9 +11,25 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        //Optimal by changing the pointer in reverse direction rather than changing data 
+        //this can be done by simple swapping of pointer 
+        //Iterative solution 
+        //TC- O(n) and SC-O(1)
+        ListNode* temp = head;
+        ListNode* prev = nullptr;
+        while(temp){
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev; // the new head of LL
+
+        
         //Brute force i can think of is using a stack to store the elements in
         // in one traversal then on 2nd pop those values from it and upfdate the list
         //TC - O(2n) and SC - O(n)
+        /*
         stack<int> st;
         ListNode* temp = head;
         while(temp){
@@ -27,5 +43,6 @@ public:
             temp = temp->next;
         }
         return head;
+        */
     }
 };
