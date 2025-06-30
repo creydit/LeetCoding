ab87817bd67321679a5473f2b0b8849b77eeb0c5
Shreyy <shreyw134@gmail.com>
2025-06-30 14:50:58 +0530

    Time: 11 ms (8.56%) | Memory: 312 MB (55.06%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
@@ -11,9 +11,29 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
+        //Optimal by using Tortoise and Hare algorithm for finding the middle of the LL
+        //with slight changes of skipping one step of slow to get it before the middle 
+        //TC - O(n/2) ~O(n) and SC - O(1)
+        if(head == NULL || head->next==NULL){
+            return NULL;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        fast = fast->next->next;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* delNode = slow->next;
+        slow->next = slow->next->next;
+        delete delNode;
+        return head;
+
         //Brute force i can think of is findng the len of LL then len/2 then we got to that node 
         //and perform the regular deleting method of nodes woth taken care of edge cases
         //TC - O(n + n/2) ~ O(n) and SC - O(1)
+
+        /*
         if(head==NULL || head->next==NULL){
             return NULL;
         }
@@ -41,5 +61,6 @@ public:
             temp = temp->next;
         }
         return head;
+        */
     }
 };
