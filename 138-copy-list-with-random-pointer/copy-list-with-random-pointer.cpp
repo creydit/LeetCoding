2a628afc0077aa2936aebadf35cd6521e59650e4
Shreyy <shreyw134@gmail.com>
2025-07-03 01:34:37 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (85.44%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,9 +17,52 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        //Optimal approach involves making new nodes and inserting between the original nodes
+        //Then connecting the random pointers to the copy ones 
+        //Then finally with the use of dummyNode connecting the next pointer in manner 
+        //to generate the new LL
+        //TC - O(3n) and SC - O(n) asked in question
+
+        //Making new nodes between old ones
+        Node* temp = head;
+        while(temp){
+            Node* copyNode = new Node(temp->val);
+            copyNode->next = temp->next;
+            temp->next = copyNode;
+            temp = temp->next->next;
+        }
+        
+        //Connecting the random pointers to the copy nodes
+        temp = head;
+        while(temp){
+            Node* copyNode = temp->next;
+            if(temp->random){
+                copyNode->random = temp->random->next;
+            }
+            else{
+                copyNode->random = nullptr;
+            }
+            temp = temp->next->next;
+        }
+
+        //Connecting the next pointer using dummy Node
+        temp = head;
+        Node* dummyNode = new Node(-1);
+        Node* res = dummyNode;
+        while(temp){
+            res->next = temp->next;
+            temp->next = temp->next->next;
+
+            res = res->next;
+            temp = temp->next;
+        }
+        return dummyNode->next;
+
         //Brute force of using a map to store both original and copy of node into it 
         //then travsering and making links from original
         //TC - O(n + n + logn) and O(2nlogn) and SC - O(n + n) ~ O(2n) for map and answer
+
+        /*
         Node* temp = head;
         unordered_map<Node*,Node*>mpp;
         while(temp){
@@ -35,5 +78,6 @@ public:
             temp = temp->next;
         }
         return mpp[head];
+        */
     }
 };
