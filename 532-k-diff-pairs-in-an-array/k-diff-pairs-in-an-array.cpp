f775eb794781b2dfb7500985db6af343323eaaa6
Shreyy <shreyw134@gmail.com>
2025-07-03 12:38:14 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (94.67%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,9 +1,38 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        //Optimal solution by using map to store and check if the need = (nums[i] - k) is there 
+        //Optimal soliution is first sorting the nums then traversing the array with two pointer
+        //and finding the perfect pairs
+        //TC - O(nlogn) and SC - O(1)
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=1;
+        int ans = 0;
+        while (i<n && j<n){
+            if (i==j){
+                j++;
+            }
+            else if(nums[j] - nums[i] < k){
+                j++;
+            }
+            else if(nums[j] - nums[i] > k){
+                i++;
+            }
+            else{
+                ans++;
+                i++;
+                while(i<n && nums[i] == nums[i-1]){
+                    i++;
+                }
+            }
+        }
+        return ans;
+
+        //Better solution by using map to store and check if the need = (nums[i] - k) is there 
         //for us if yes that means we got a pair
-        // TC - O(n) and SC - O(n)
+        // TC - O(n) but if chaining occurs then O(n2) and SC - O(n)
+        /*
         int n = nums.size();
         unordered_map<int,int> mpp;
         int ans = 0;
@@ -22,6 +51,7 @@ public:
             }
         }
         return ans;
+        */
 
         //Brute force can be using two loops to check every pair of elments in nums
         // TC - O(n2) and SC - O(1)
