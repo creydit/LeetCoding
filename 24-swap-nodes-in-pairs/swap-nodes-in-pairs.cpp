90c636259674b4fe9913652d5929c2ecef12a973
Shreyy <shreyw134@gmail.com>
2025-07-05 01:21:14 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (19.44%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -11,9 +11,34 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
+        //Optimal solution by changing link of nodes in pairs with use of dummy node and 4 pointer
+        //pointing to prev , curr (temp) and 2 fronts next and next to next
+        // TC - O(n) and SC - O(1)
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* temp = head;
+        ListNode* prev = dummy;
+        while(temp && temp->next){
+            ListNode* front1 = temp->next;
+            ListNode* front2 = temp->next->next;
+            front1->next = temp;
+            temp->next = front2;
+            prev->next = front1;
+
+            prev = temp;
+            temp = front2;
+        }
+        return dummy->next;
+
+
         //Brute force i can think of is using a array to store the values then swapping adjacent 
         //then again replacing it in the LL
         //TC O(n+n+n) ~ O(3n) and SC - O(n)
+
+        /*
         if(head == NULL || head->next == NULL){
             return head;
         }
@@ -34,5 +59,6 @@ public:
             temp = temp->next;
         }
         return head;
+        */
     }
 };
