dfc4e032b1a170eee23f11cb4192ffe124794470
Shreyy <shreyw134@gmail.com>
2025-07-05 14:50:27 +0530

    Time: 1 ms (87.11%) | Memory: 13.3 MB (77.31%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<string> ans;
+    /*
     bool isValid(string &s){
         int cnt=0;
         for(int i=0;i<s.size();i++){
@@ -32,12 +33,38 @@ public:
         solve(curr,n);
         curr.pop_back();
     }
+    */
+    void solve(string &curr, int n,int open,int close){
+        if(curr.size() == 2*n){
+            ans.push_back(curr);
+            return;
+        }
 
+        if(open < n){
+            curr.push_back('(');
+            solve(curr, n , open+1, close);
+            curr.pop_back();   
+        }
+        if(close < open){
+            curr.push_back(')');
+            solve(curr, n , open, close+1);
+            curr.pop_back();
+        }
+    }
     vector<string> generateParenthesis(int n) {
+        //Better Solution same approach but different condition
+        //TC - O(2^2n) and SC - O(2n) depth of recursion
+        string s;
+        int open = 0;
+        int close = 0;
+        solve(s,n,open,close);
+        return ans;
         //Recursion approach and Backtracking
         //TC - O(2^2n * 2n) and SC - O(2*n) for depth of recursion
+        /*
         string s;
         solve(s,n);
         return ans;
+        */
     }
 };
