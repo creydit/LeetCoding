bd91b2a78dbc3be65893ccad2aaffc4a2eb239c2
Shreyy <shreyw134@gmail.com>
2025-07-11 01:56:06 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (86.46%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,9 +1,29 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        //Optimal approach by using sliding window and using a hash map to move the window by required 
+        //lenght and check if we have seen it or not
+        //TC - O(n) and SC - O(1) as O(256) ~ O(1)
+        int n = s.size();
+        int ans = 0;
+        int l = 0;
+        int r = 0;
+        vector<int> hash(256, -1);
+        while(r < n){
+            char c = s[r];
+            if(hash[c] != -1 && hash[c] >= l){
+                l = hash[c] + 1;
+            }
+            hash[c] = r;
+            ans = max(ans, r - l + 1);
+            r++;
+        }
+        return ans;
+
         //Better by just using 2 loops and not exactly creating sub strings just traversing  
         //through it using the 2nd loop and checking if we have seen a character by using map
-        //TC - O(n2) and SC - O(n)
+        //TC - O(n2) and SC - O(1) as at worst O(26)
+        /*
         int n = s.size();
         int ans = 0;
         for(int i = 0 ; i < n; i++){
@@ -17,11 +37,14 @@ public:
             }
         }
         return ans;
+        */
+
+
         //Brute force approch making all the substring from s using 2 loops and then
         //traversing through that substring if we find a duplicate character we break and leave that
         //substring and checking of duplication is done by using a map 
         //also we update the ans with max of new string if it is
-        // TC - O(n3) amd SC - O(n+n)
+        // TC - O(n3) amd SC - O(n)
         /*
         int n = s.size();
         int ans = -1;
