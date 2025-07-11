cb216da49812d788dc3756dd45b5ce2de2edad19
Shreyy <shreyw134@gmail.com>
2025-07-12 02:35:23 +0530

    Time: 4 ms (58.03%) | Memory: 15 MB (46.03%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.cpp
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.cpp
@@ -4,14 +4,13 @@ public:
         //Unreal magic, SEE EDITORIAL
         //TC - O(n) and sC - O(1)
         int n = s.size();
-        int ans = 0;
-        int curr = 0;
-        for(int i = 0; i < n; i++){
-            if(i==0 || s[i] == s[i-1]){
-                curr++;
-            }
+        int ans = 1;
+        int curr = 1;
+        int m = 1000000007;
+        for(int i = 1; i < n; i++){
+            if(s[i] == s[i-1])curr++;
             else curr = 1;
-            ans = (ans + curr)%1000000007;
+            ans = (ans + (curr%m) ) % m;
         }
         return ans;
     }
