686c5821e779660e9a43cb79a04e6c83b0d927ef
Shreyy <shreyw134@gmail.com>
2025-07-14 23:53:41 +0530

    Time: 3 ms (66.64%) | Memory: 8.1 MB (17.18%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
+    /*
     int solve(int n, vector<int> &dp){
         if (n <= 1) return n;
         if(dp[n] != -1) return dp[n];
         return dp[n] = solve(n-1, dp) + solve(n-2, dp);
     }
-
+    */
     int fib(int n) {
+        //More Better by tabulation and DP
+        //Bottom Up
+        if(n==0) return 0;
+        vector<int> dp(n+1, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for( int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+        
         //Better by using memoization and Dp 
         //Top Down
+        //TC - O(n) and SC - O(n + n) for recursion stack space and dp array
+        /*
         vector<int> dp(n+1, -1);
         return solve(n, dp); 
+        */
 
         //Brute force using plane recursion
         //TC - O(2^n) and SC - O(n) recursion stack space
