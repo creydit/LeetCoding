990a05ba981a430ebd677c82bd94f966c2b57b68
Shreyy <shreyw134@gmail.com>
2025-07-17 02:44:22 +0530

    Time: 8 ms (59.27%) | Memory: 11.1 MB (55.98%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
+        //Optimal solution by slding window and 2 pointer 
+        //we are making a least possible sub string which has all 3 chars and then adding the previous
+        //all the elements of it
+        //TC - O(n) and SC - O(1)
+        vector<int> lastSeen(3, -1);
+        int ans = 0;
+        for(int i = 0; i < s.size(); i++){
+            lastSeen[s[i] - 'a'] = i;
+            if(lastSeen[0] != -1 && lastSeen[1] != -1 &&lastSeen[2] != -1 ){
+                ans += 1 + *min_element(lastSeen.begin(), lastSeen.end());
+            }
+        }
+        return ans;
+
         //bEtter by little change when we get a all three chars further every string will have them
         //so we can break from there
         //TC - O(n^2) and SC- O(1)
+        /*
         int n = s.size();
         int ans = 0;
         for(int i = 0; i< n; i++){             
@@ -21,6 +36,7 @@ public:
             }
         }
         return ans;
+        */
 
         //Brute force using 2 loops scanning throught all the sub string and checking the desired thing
         //TC - O(n^2) and SC - O(1)
