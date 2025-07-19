77058eebf2dacbdedfcff7b07b7b77f7172e4908
Shreyy <shreyw134@gmail.com>
2025-07-19 13:48:47 +0530

    Time: 77 ms (5.04%) | Memory: 138.2 MB (5.01%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -3,8 +3,8 @@ public:
     void dfs(int at,  vector<vector<int>> aL, vector<bool> &vis){
         vis[at] = true;
         for(int j = 0; j < aL.size(); j++){
-            if(aL[at][j] == 1 && !vis[j]){
-                dfs(j, aL, vis);
+            if(aL[at][j] == 1){
+                if(!vis[j]) dfs(j, aL, vis);
             }
         }
     }
@@ -12,16 +12,6 @@ public:
         //TC - O(N + V + 2E) and SC - O( N + N) for vis arrays and recursion stack space 
         int n = isConnected.size();
         vector<bool> vis(n, false);
-        //Conversion of Adjacency matrix to List
-        /*
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(isConnected[i][j] == 1 && i != j){
-                    adjList[i].push_back(j);
-                }
-            }
-        }
-        */
 
         //Simple algorithm to count the number of different DFS calls made thart will the num of provinces
         int ans = 0;
