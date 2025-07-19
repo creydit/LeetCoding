51d5de38466109886e0b6e8b9d568c3bf29a3ab1
Shreyy <shreyw134@gmail.com>
2025-07-19 19:42:55 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (77.43%) - LeetSync

---
+++ 733-flood-fill/flood-fill.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private: 
+    void dfs(int sr, int sc, vector<vector<int>>& ans, vector<vector<int>>& image, int iniC, int nC){
+        int n = image.size();
+        int m = image[0].size();
+        ans[sr][sc] = nC;
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, 1, 0, -1};
+
+        for(int d = 0; d < 4; d++){
+            int rr = sr + dx[d];
+            int cc = sc + dy[d];
+            
+            if( rr>=0 && cc>=0 && rr < n && cc < m && ans[rr][cc] != nC and image[rr][cc] == iniC ){
+                dfs(rr, cc, ans, image, iniC, nC);
+            }
+        }
+    }
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        //DFS 
+        //TC - O( 4*(n*m)) and SC - O(2 *(n*M))
+        vector<vector<int>> ans = image;
+        int iniC = image[sr][sc];
+        dfs(sr, sc, ans, image, iniC, color);
+        return ans;
+    }
+};
