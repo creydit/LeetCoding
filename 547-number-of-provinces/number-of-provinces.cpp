57644517affb36b3bf79253459cabb562af4c01b
Shreyy <shreyw134@gmail.com>
2025-07-19 13:46:20 +0530

    Time: 86 ms (5.04%) | Memory: 138.2 MB (5.01%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void dfs(int at,  vector<vector<int>> aL, vector<bool> &vis){
+        vis[at] = true;
+        for(int j = 0; j < aL.size(); j++){
+            if(aL[at][j] == 1 && !vis[j]){
+                dfs(j, aL, vis);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        //TC - O(N + V + 2E) and SC - O( N + N) for vis arrays and recursion stack space 
+        int n = isConnected.size();
+        vector<bool> vis(n, false);
+        vector<vector<int>> adjList(n);
+
+        //Conversion of Adjacency matrix to List
+        /*
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(isConnected[i][j] == 1 && i != j){
+                    adjList[i].push_back(j);
+                }
+            }
+        }
+        */
+
+        //Simple algorithm to count the number of different DFS calls made thart will the num of provinces
+        int ans = 0;
+        for(int i = 0; i< n; i++){
+            if(!vis[i]){
+                ans++;
+                dfs(i, isConnected, vis);
+            }
+        }
+        return ans;
+    }
+};
