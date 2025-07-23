bc89f073373474ceff728fc155360501fcef1f90
Shreyy <shreyw134@gmail.com>
2025-07-23 23:56:26 +0530

    Time: 80 ms (65.05%) | Memory: 56.2 MB (43.11%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int solve(vector<int> &nums, int k){
         //TC - O(2n)
-        int n = nums.size();
         int r = 0;
         int l = 0;
-        map<int, int>mpp;
+        unordered_map<int, int>mpp;
         int ans = 0;
-        while( r < n){
+        while( r < nums.size()){
             mpp[nums[r]]++;
             while(mpp.size() > k){
                 mpp[nums[l]]--;
                 if(mpp[nums[l]] == 0) mpp.erase(nums[l]);
                 l++;
             }
-            ans += r - l + 1;
+            ans = ans + (r - l + 1);
             r++;
         }
         return ans;
@@ -24,7 +23,7 @@ public:
         //TC - O(4*n) and SC - O(n)
         return solve(nums, k) - solve(nums, k-1);
 
-        
+
         //Brute force using 2 loops 
         //TC - O(n2) and SC - O(n)
         /*
