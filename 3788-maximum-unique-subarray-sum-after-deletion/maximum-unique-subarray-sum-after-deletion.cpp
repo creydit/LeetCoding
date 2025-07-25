e9518a0f15d08eecbf54d7587da8a0120c99d6b0
Shreyy <shreyw134@gmail.com>
2025-07-25 20:13:51 +0530

    Time: 1 ms (37.33%) | Memory: 29.8 MB (23.82%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
@@ -5,18 +5,15 @@ public:
         // TC - O(n) and SC - O(101)
         int ans = 0;
         vector<int> hash(101);
+        int maxi = INT_MIN;
         for(auto i : nums){
             if ( i > 0 && hash[i] == 0){
                 ans += i;
                 hash[i]++;
             } 
+            maxi = max(maxi, i);
         }
-        if (ans == 0){
-            ans = INT_MIN;
-            for(auto i : nums){
-                ans = max(ans, i);
-            }
-        }
+        if (ans == 0) return maxi;
         return ans;
     }
 };
