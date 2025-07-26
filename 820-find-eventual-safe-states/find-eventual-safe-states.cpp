13d55099adb55ecb0aeb10060c18758b48fa7850
Shreyy <shreyw134@gmail.com>
2025-07-26 22:15:52 +0530

    Time: 53 ms (34.65%) | Memory: 66.4 MB (21.09%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     bool dfs(int at, vector<bool> &vis, vector<bool> &pathVis, vector<vector<int>> &graph, vector<bool> &safeNode){
         vis[at] = true;
         pathVis[at] = true;
@@ -15,9 +16,44 @@ class Solution {
         pathVis[at] = false;
         return false;
     }
+    */
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        //BY BFS and Topo Sort
+        //Reversing the graph and performing kahn's algorithm
+        int n = graph.size();
+        vector<vector<int>> revG(n);
+        vector<int> indegree(n,0);
+        for(int i = 0; i < n ; i++){         //i -> node
+            for(auto &node : graph[i]){
+                revG[node].push_back(i);    // node -> i
+                indegree[i]++;
+            }
+        }
+        queue<int> q;
+        for(int i = 0; i<n ; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int at = q.front();
+            q.pop();
+            ans.push_back(at);
+
+            for(auto next : revG[at]){
+                indegree[next]--;
+                if( indegree[next] == 0){
+                    q.push(next);
+                }
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+        
         //BY DFS detecting cycle and all those nodes connected to cycle will not be safe nodes
+        /*
         int n = graph.size();
         vector<bool> vis(n, false);
         vector<bool> pathVis(n,false);
@@ -33,5 +69,6 @@ public:
             if(safeNode[i]) ans.push_back(i);
         }
         return ans;
+        */
     }
 };
