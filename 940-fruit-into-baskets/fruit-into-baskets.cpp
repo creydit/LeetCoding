80ad21a3cbc180cef6e4f466faa61715a9fcafbf
Shreyy <shreyw134@gmail.com>
2025-07-26 11:45:26 +0530

    Time: 36 ms (71.44%) | Memory: 74.9 MB (76.92%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,8 +1,28 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
+        //Optimal solution by changes in sliding window solution
+        //TC - O(n) and SC - O(3)
+        int n = fruits.size();
+        int maxLen = 0;
+        int l = 0;
+        int r = 0;
+        unordered_map<int, int> mpp;
+        while( r < n){
+            mpp[fruits[r]]++;
+            if( mpp.size() > 2){
+                mpp[fruits[l]]--;
+                if( mpp[fruits[l]] == 0) mpp.erase(fruits[l]);
+                l++;
+            }
+            maxLen = max(maxLen, r - l  + 1);
+            r++;
+        }
+        return maxLen;
+
         //Better by sliding window approach using a map to store atmost 2 elements
         //TC - O(n + n) and SC - O(3)
+        /*
         int n = fruits.size();
         int maxLen = 0;
         int l = 0;
@@ -19,6 +39,7 @@ public:
             r++;
         }
         return maxLen;
+        */
 
         //Brute force (navie) solution is using 2 loops to calculate all the sub arrays 
         //and finding out the maximum length
