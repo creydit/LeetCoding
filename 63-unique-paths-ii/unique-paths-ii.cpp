67acca6bc144c3c208b7134faa55418ac6ae7dff
Shreyy <shreyw134@gmail.com>
2025-08-04 20:10:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (54.37%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(int r, int c, vector<vector<int>> &grid, vector<vector<int>>& dp){
+        if(r>=0  && c>=0 && grid[r][c] == 1){
+            return 0;
+        }
+        if (r==0 && c==0) return 1;
+        if ( r < 0 || c < 0) return 0;
+
+        if(dp[r][c] != -1) return dp[r][c];
+
+        int up = solve(r-1, c, grid, dp);
+        int left = solve(r, c-1, grid, dp);
+
+        return dp[r][c] = up + left;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        //Recursion + DP woth memoization
+        //TC - O(n*m) and SC - (n+m+n*m)
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return solve(n-1, m-1, obstacleGrid, dp);
+    }
+};
