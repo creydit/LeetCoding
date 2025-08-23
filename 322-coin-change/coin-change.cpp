0ed7b74f68341ee50de01df3e828932844953a54
Shreyy <shreyw134@gmail.com>
2025-08-23 19:09:41 +0530

    Time: 39 ms (52.00%) | Memory: 22.1 MB (53.90%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -16,8 +16,29 @@ public:
     }
     */
     int coinChange(vector<int>& coins, int amount) {
+        //Space Optimised 
+        //TC - O(n*amt) and SC - O(amt)
+        int n = coins.size();
+        vector<int> prev(amount+1, 0), curr(amount+1, 0);
+        for(int T = 0; T <= amount; T++){
+            if (T % coins[0] == 0) prev[T] = T / coins[0];
+            else prev[T] = 1e9;
+        }
+        for(int i = 1; i < n; i++){
+            for(int T = 0; T <= amount; T++){
+                int  nottake = prev[T];
+                int take = 1e9;
+                if (coins[i] <= T) take = 1 + curr[T - coins[i]];
+                curr[T] = min(take, nottake);
+            }
+            prev = curr;
+        }
+        int ans = prev[amount];
+        if (ans >= 1e9) return -1;
+        return ans;
         //By Tabulation 
         //TC - O(n*amt) and SC - O(n*amt)
+        /*
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int> (amount+1, 0));
         for(int T = 0; T <= amount; T++){
@@ -35,6 +56,7 @@ public:
         int ans = dp[n - 1][amount];
         if (ans >= 1e9) return -1;
         return ans;
+        */
         //By Recursion + DP + Memoization //Top Down
         //TC - O(n*amt) and SC - O(n*amt + n)
         /*
