f5484a2b0da88063e92099147963c230aeade48a
Shreyy <shreyw134@gmail.com>
2025-08-23 19:03:17 +0530

    Time: 47 ms (36.95%) | Memory: 48.7 MB (47.99%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int solve(int idx, vector<int> &arr, int T, vector<vector<int>> &dp){
         if(idx == 0){
             if ( T % arr[idx] == 0) return T/arr[idx];
@@ -13,13 +14,35 @@ public:
 
         return dp[idx][T] = min(take, nottake);
     }
+    */
     int coinChange(vector<int>& coins, int amount) {
+        //By Tabulation 
+        //TC - O(n*amt) and SC - O(n*amt)
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int> (amount+1, 0));
+        for(int T = 0; T <= amount; T++){
+            if (T % coins[0] == 0) dp[0][T] = T / coins[0];
+            else dp[0][T] = 1e9;
+        }
+        for(int i = 1; i < n; i++){
+            for(int T = 0; T <= amount; T++){
+                int  nottake = dp[i-1][T];
+                int take = 1e9;
+                if (coins[i] <= T) take = 1 + dp[i][T - coins[i]];
+                dp[i][T] = min(take, nottake);
+            }
+        }
+        int ans = dp[n - 1][amount];
+        if (ans >= 1e9) return -1;
+        return ans;
         //By Recursion + DP + Memoization //Top Down
         //TC - O(n*amt) and SC - O(n*amt + n)
+        /*
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int> (amount+1, -1));
         int ans = solve(n - 1, coins, amount, dp);
         if (ans >= 1e9) return -1;
         return ans;
+        */
     }
 };
