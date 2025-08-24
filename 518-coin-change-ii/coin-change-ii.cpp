15bc19c7e36b635103ca03dfd56cd171ff19649a
Shreyy <shreyw134@gmail.com>
2025-08-24 13:55:46 +0530

    Time: 37 ms (12.96%) | Memory: 80.6 MB (7.13%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int solve(int idx, vector<int> &coins, int target, vector<vector<int>> &dp){
         if(idx == 0){
             if( target % coins[0] == 0) return 1;
@@ -13,12 +14,35 @@ public:
 
         return dp[idx][target] = take + nottake;
     }
+    */
     int change(int amount, vector<int>& coins) {
-        //By coin change logic instead if just calculating money we just count it in 1 way to apporach answer
+        //By coin change logic instead if just calculating money we just count it in 
+
+        //By Tabultion
+        //TC - O(n*amount) and SC - O(n*amount)
+        using u128 = unsigned __int128;
+        int n = coins.size();
+        vector<vector<u128>> dp(n, vector<u128>(amount+1, 0));
+        for(int target = 0; target <= amount; target++){
+            if (target % coins[0] == 0) dp[0][target] = 1;
+        }
+        for(int idx = 1; idx < n; idx++){
+            for(int target = 0; target <= amount; target++){
+                u128 nottake = dp[idx - 1][target];
+                u128 take = 0;
+                if (coins[idx] <= target) take = dp[idx][target - coins[idx]];
+                dp[idx][target] = take + nottake;
+            }
+        }
+        return (int)dp[n-1][amount];
+
+        //1 way to apporach answer
         //By Recursion + DP + memoization
-        //TC - O(n*amount) and SC - O(n*amount + 1)
+        //TC - O(n*amount) and SC - O(n*amount + n)
+        /*
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int>(amount+1, -1));
         return solve(n-1, coins, amount, dp);
+        */
     }
 };
