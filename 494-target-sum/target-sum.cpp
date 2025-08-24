4c83fccd5ebac71651b732881f6817f1ef54c4ec
Shreyy <shreyw134@gmail.com>
2025-08-24 13:03:14 +0530

    Time: 1 ms (91.78%) | Memory: 15.5 MB (48.42%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int solve(int idx, int sum, vector<int>& nums, vector<vector<int>> &dp){
         if(idx == 0){
             if (sum == 0 && nums[0] == 0) return 2;
@@ -15,17 +16,45 @@ public:
 
         return dp[idx][sum] = take + nottake;
     }
+    */
+    int tabu(vector<int> nums, int target){
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(target + 1, 0));
+        if(nums[0] == 0) dp[0][0] = 2;
+        else{
+            dp[0][0] = 1;
+            if(nums[0] <= target) dp[0][nums[0]] = 1;
+        }
+        for(int idx = 1; idx < n; idx++){
+            for(int sum = 0; sum <= target; sum++){
+                int nottake = 0 + dp[idx - 1][sum];
+                int take = 0;
+                if(nums[idx] <= sum) take += dp[idx - 1][sum - nums[idx]];
 
+                dp[idx][sum] = take + nottake;
+            }
+        }
+        return dp[n-1][target];
+    }
     int findTargetSumWays(vector<int>& nums, int target) {
         //By deducing this problem becomes "Partitions with Given Difference" and so does the code
-        //Tc - O(n*target) and SC - O(n*target + n)
+       
         int totsum = 0;
         for(auto i : nums) totsum += i;
         if(totsum - target < 0 || (totsum - target) % 2 != 0) return 0;
         int n = nums.size();
         int newTarget = (totsum - target) / 2;
+        
+        //BY Tabulation
+        //TC - O(n*target) and SC - O(n*target)
+        return tabu(nums, newTarget);
+
+        //By Recursion + DP + Memoization
+        //Tc - O(n*target) and SC - O(n*target + n)
+        /*
         vector<vector<int>> dp(n, vector<int> (newTarget+1 , -1));
         return solve(n-1, newTarget, nums, dp);
+        */
 
     }
 };
