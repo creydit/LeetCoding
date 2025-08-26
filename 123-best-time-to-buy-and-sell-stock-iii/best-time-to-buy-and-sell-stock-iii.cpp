f9ab59cf01b385f3e6b66a1eb1e861588645693b
Shreyy <shreyw134@gmail.com>
2025-08-26 10:49:55 +0530

    Time: 55 ms (71.67%) | Memory: 79.5 MB (80.25%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int solve(int idx, int buy, int cap, vector<int> &prices, vector<vector<vector<int>>> &dp){
         if (cap == 0) return 0;
         if (idx == prices.size()) return 0;
@@ -17,11 +18,67 @@ public:
         }
         return dp[idx][buy][cap] = profit;
     }
+    */
+
     int maxProfit(vector<int>& prices) {
+        //Space Optimised
+        //TC - O(n*2*3) and SC - O(2*3 )
+        int n = prices.size();
+        vector<vector<int>> after(2, vector<int> (3, 0));
+        vector<vector<int>> curr(2, vector<int> (3, 0));
+        //Base Cases are already zero
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cap = 1; cap <= 2; cap++){
+                    int profit = 0;
+                    if(buy){
+                        profit = max( -prices[idx] + after[0][cap], 
+                                    0 + after[1][cap] );
+                    }
+                    else{
+                        profit = max( prices[idx] + after[1][cap-1] ,
+                                        0 + after[0][cap] );
+                    }
+                    curr[buy][cap] = profit;
+                }
+            }
+            after = curr;
+        }
+        return after[1][2];
+
+        //By Tabulation
+        //TC - O(n*2*3) and SC - O(n*2*3)
+        /*
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3, 0)));
+        //Base CAses are already zero
+
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cap = 1; cap <= 2; cap++){
+                    int profit = 0;
+                    if(buy){
+                        profit = max( -prices[idx] + dp[idx + 1][0][cap], 
+                                    0 + dp[idx +  1][1][cap] );
+                    }
+                    else{
+                        profit = max( prices[idx] + dp[idx + 1][1][cap-1] ,
+                                        0 + dp[idx + 1][0][cap] );
+                    }
+                    dp[idx][buy][cap] = profit;
+                }
+            }
+        }
+        return dp[0][1][2];
+        */
+
+
         //By Recursion + DP + Memoization  // Top Down
         //TC - O(n*2*3) and SC - O(n*2*3 + n)
+        /*
         int n = prices.size();
         vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int> (3, -1)));
         return solve(0, 1, 2, prices, dp);
+        */
     }
 };
