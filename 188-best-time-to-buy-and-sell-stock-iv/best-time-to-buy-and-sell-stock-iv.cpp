41befc79c1b9c41a7447b8f6b3e1f1515e0340c2
Shreyy <shreyw134@gmail.com>
2025-08-27 14:19:14 +0530

    Time: 3 ms (86.16%) | Memory: 14.2 MB (95.16%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int solve(int idx, int trans, vector<int>& prices, int n, int k, vector<vector<int>> &dp){
         if (idx == n || trans == 2*k ) return 0;
 
@@ -16,16 +17,70 @@ public:
         return dp[idx][trans] = profit;
 
     }
+    */
     int maxProfit(int k, vector<int>& prices) {
         //similar problem to buy and sell stocks 3 just there were only two transactions allowed 
         //here we have k allowed transactions
 
         //Solving with another approach which is using transaction number that will be even for buying
         //and odd for selling 
+
+        //Space Optimised
+        //TC - O(n*2k) and SC- O(2k)
+        int n = prices.size();
+        vector<int> after (2*k + 1, 0);
+        vector<int> curr (2*k + 1, 0);
+
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int trans = 2*k - 1; trans >= 0; trans--){
+                int profit = 0;
+                if (trans%2 == 0){
+                    profit = max( -prices[idx] + after[trans + 1] , 
+                                    0 + after[trans] );
+                }
+                else{
+                    profit = max( prices[idx] + after[trans + 1],
+                                    0 + after[trans] );
+                }
+                curr[trans] = profit;
+            }
+            after = curr;
+        }
+        return after[0];
+
+
+        //tabulation
+        //TC - O(n*2k) and SC- O(n*2k)
+        /*
+        int n = prices.size();
+        vector<vector<int>> dp(n+1 , vector<int> (2*k + 1, 0));
+
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int trans = 2*k - 1; trans >= 0; trans--){
+                int profit = 0;
+                if (trans%2 == 0){
+                    profit = max( -prices[idx] + dp[idx+1][trans + 1] , 
+                                    0 + dp[idx+1][trans] );
+                }
+                else{
+                    profit = max( prices[idx] + dp[idx+1][trans + 1],
+                                    0 + dp[idx + 1][trans] );
+                }
+                dp[idx][trans] = profit;
+            }
+        }
+        return dp[0][0];
+        */
+
+
+        //Recursion + Dp + memoization
         // TC - O(n*2k) and SC - O(n*2k + n)
+        /*
         int n = prices.size();
         vector<vector<int>> dp(n , vector<int> (2*k, -1));
         return solve(0, 0, prices, n, k, dp);
+        */
+
 
 
         //solving direct space optimsied version //refer BAS3 for more detials
