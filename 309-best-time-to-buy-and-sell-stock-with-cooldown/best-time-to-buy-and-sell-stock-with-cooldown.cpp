da0d0e30962642d7b7667fb255ee9bdf5d3bbe1c
Shreyy <shreyw134@gmail.com>
2025-08-27 14:44:54 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (88.94%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -22,8 +22,36 @@ public:
         //almost same as buy and sell stock 2 with only difference of cooldown which can be
         //done by skipping 1 index after selling 
         
+        //Space OPtimisation
+        // TC - O(2*n) and SC - O(6)
+        int n = prices.size();
+        vector<int> f2(2, 0);
+        vector<int> f1(2, 0);
+        vector<int> curr(2, 0);
+
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int buy = 0; buy <= 1; buy++){
+                int profit = 0;
+                if (buy){
+                    profit = max( -prices[idx] + f1[0],
+                                    0  + f1[1] );
+                }
+                else{
+                    profit = max( prices[idx] + f2[1],
+                                    0 + f1[0] );
+                }
+
+                curr[buy] = profit;
+            }
+            f2 = f1;
+            f1 = curr;
+        }
+        return f1[1];
+        
+        
         //by tabulation
         // TC - O(2*n) and SC - O(2*n)
+        /*
         int n = prices.size();
         vector<vector<int>> dp(n+2, vector<int>(2, 0));
 
@@ -43,6 +71,7 @@ public:
             }
         }
         return dp[0][1];
+        */
 
         //by Recursion + DP + memoization
         // TC - O(2*n) and SC - O(2*n + n)
