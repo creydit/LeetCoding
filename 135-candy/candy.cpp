4bf07cde1653a1a51bb43eea3577fae939c95e0e
Shreyy <shreyw134@gmail.com>
2025-08-31 21:56:44 +0530

    Time: 2 ms (30.55%) | Memory: 23.3 MB (21.08%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        //Brute Force using both directional traversal and keeping arrays to give candies
+        //then taking max of both the answer
+        //TC - O(3n) and SC - O(2n)
+        int n = ratings.size();
+        vector<int> left(n , 0);
+        vector<int> right(n, 0);
+        left[0] = 1;
+        right[n-1] = 1;
+        for(int i = 1; i < n; i++){
+            if( ratings[i] > ratings[i-1]){
+                left[i] = left[i-1] + 1;
+            }
+            else{
+                left[i] = 1;
+            }
+        }
+
+        for(int i = n-2; i >= 0; i--){
+            if( ratings[i] > ratings[i+1]){
+                right[i] = right[i+1] + 1;
+            }
+            else{
+                right[i] = 1;
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            ans += max(left[i], right[i]);
+        }
+        return ans;
+
+
+    }
+};
