e64a428d84f012b927a3a65f98f1a266e1642dc2
Shreyy <shreyw134@gmail.com>
2026-01-31 18:37:06 +0530

    Time: 8 ms (85.54%) | Memory: 22.1 MB (17.14%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -4,6 +4,25 @@ class Solution:
         m = len(grid[0])
         dp = [[-1 for _ in range(m)] for __ in range(n)]
 
+        for i in range(n):
+            for j in range(m):
+                if i==0 and j==0:
+                    dp[i][j] = grid[i][j]
+                else:
+                    up = grid[i][j] 
+                    if j > 0:
+                        up += dp[i][j-1]
+                    else:
+                        up = 1e9
+                    
+                    left = grid[i][j]
+                    if i > 0:
+                        left += dp[i-1][j]
+                    else:
+                        left = 1e9
+                    dp[i][j] = min(up,left)
+                
+        '''
         def solve(r, c):
             if r == n-1 and c == m-1:
                 return grid[r][c]
@@ -16,6 +35,6 @@ class Solution:
             
             dp[r][c] = grid[r][c] + min(solve(r+1,c), solve(r,c+1))
             return dp[r][c]
-        
-        ans = solve(0,0)
+        '''
+        ans = dp[n-1][m-1]
         return ans 
