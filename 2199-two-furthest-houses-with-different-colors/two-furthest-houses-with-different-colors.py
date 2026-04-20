8bf36e2767a6a6564190260a11a1ffb44ee224d1
Shreyy <shreyw134@gmail.com>
2026-04-20 20:06:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (61.41%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.py
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.py
@@ -1,6 +1,16 @@
 class Solution:
     def maxDistance(self, colors: List[int]) -> int:
+        n = len(colors)
+        ans = -int(1e9)
+        for i in range(n):
+            if colors[i] != colors[0]:
+                ans = max(ans, i)
+            if colors[i] != colors[-1]:
+                ans = max(ans, n-1-i)
+        return ans
+
         #Brute Force - O(n2)
+        '''
         ans = -int(1e9)
         n = len(colors)
         for i in range(n):
@@ -8,3 +18,4 @@ class Solution:
                 if colors[i] != colors[j]:
                     ans = max(ans, j - i)
         return ans 
+        '''
