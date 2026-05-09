bba092ea9d37888f1a01339aa1390958e6234ffc
Shreyy <shreyw134@gmail.com>
2026-05-09 23:17:01 +0530

    Time: 1155 ms (70.84%) | Memory: 19.5 MB (77.03%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.py
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.py
@@ -3,6 +3,27 @@ class Solution:
         n = len(nums)
         ans = 0
         for i in range(n):
+            seen = set()
+            diff = 0
+            for j in range(i,n):
+                x = nums[j]
+                if x not in seen:
+                    seen.add(x)
+                    if x%2==0:
+                        diff+=1
+                    else:
+                        diff-=1
+                if diff==0:
+                    ans = max(ans,j-i+1)
+        return ans
+
+
+        '''
+        #Better - Brute Force
+        #O(n2)
+        n = len(nums)
+        ans = 0
+        for i in range(n):
             evens = set()
             odds = set()
             for j in range(i,n):
@@ -13,3 +34,4 @@ class Solution:
                 if len(evens)==len(odds):
                     ans = max(ans, j-i+1)
         return ans
+        '''
