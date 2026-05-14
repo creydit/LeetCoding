7b34ab754ba450368e259993529ec841d25ce842
Shreyy <shreyw134@gmail.com>
2026-05-14 18:17:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (59.43%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.py
+++ 2892-check-if-array-is-good/check-if-array-is-good.py
@@ -1,5 +1,15 @@
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
+        #by sorting
+        nums.sort()
+        n = len(nums)
+        for i in range(n-1):
+            if nums[i] != i+1:
+                return False
+        return nums[n-1] == n-1
+
+        #by Me
+        '''
         n = len(nums)
         mx = max(nums)
         base = [i for i in range(1,mx+1)]
@@ -7,3 +17,4 @@ class Solution:
         if sorted(nums)!=base:
             return False
         return True
+        '''
