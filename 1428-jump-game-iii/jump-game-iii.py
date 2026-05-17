4ffa3ec98d5dc1ad85aa4875116ffacfe6eb09bb
Shreyy <shreyw134@gmail.com>
2026-05-17 22:18:48 +0530

    Time: 7 ms (97.28%) | Memory: 25.2 MB (92.68%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.py
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -1,5 +1,31 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
+        #BFS 
+        n = len(arr)
+        vis = [0]*(n+1)
+        q = deque()
+        q.append(start)
+        vis[start] = 1
+        while q:
+            at = q.popleft()
+
+            if arr[at] == 0:
+                return True
+
+            left = at - arr[at]
+            right = at + arr[at]
+
+            if left >= 0 and vis[left] == 0:
+                vis[left] = 1
+                q.append(left)
+            if right < n and vis[right] == 0:
+                vis[right] = 1
+                q.append(right)
+        return False
+
+
+        #DFS
+        '''
         n = len(arr)
         vis = [0] *(n+1)
         def solve(idx):
@@ -17,3 +43,4 @@ class Solution:
             return ll or rr
 
         return solve(start)
+        '''
