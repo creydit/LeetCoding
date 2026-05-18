b1d86cf7d32cb06462045cc8e84b66db7c4cd3f5
Shreyy <shreyw134@gmail.com>
2026-05-18 13:21:36 +0530

    Time: 196 ms (42.74%) | Memory: 33.6 MB (74.41%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        n = len(arr)
+        dic = defaultdict(list)
+        for i in range(n):
+            dic[arr[i]].append(i)
+        vis = [0]*(n+1)
+
+        q = deque()
+        q.append((0,0))
+        vis[0] = 1
+        ans = 10**18
+        while q:
+            node = q.popleft()
+            at = node[0]
+            jumps = node[1]
+            if at == n-1:
+                ans = min(ans,jumps)
+
+            m1 = at + 1
+            if m1 < n and vis[m1] == 0:
+                q.append((m1,jumps+1))
+                vis[m1] = 1
+
+            m2 = at - 1
+            if m2 >= 0 and vis[m2] == 0:
+                q.append((m2,jumps+1))
+                vis[m2] = 1
+            
+
+            for m in dic[arr[at]]:
+                if m != at:
+                    m3 = m
+                    if vis[m3] == 0:
+                        q.append((m3,jumps+1))
+                        vis[m3] = 1
+            dic[arr[at]] = []
+        return ans
+            
