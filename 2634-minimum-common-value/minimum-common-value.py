a79b674ad41a9f4a23df43216e5f1cca674aafda
Shreyy <shreyw134@gmail.com>
2026-05-19 14:12:03 +0530

    Time: 0 ms (100.00%) | Memory: 37.8 MB (51.94%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -2,6 +2,10 @@ class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
         #Optimal
         #TC - O(n^2)
+        if nums1[-1] < nums2[0]:
+            return -1
+        if nums2[-1] < nums1[0]:
+            return -1
         n1 = len(nums1)
         n2 = len(nums2) 
         i = 0
@@ -23,6 +27,7 @@ class Solution:
             for j in range(n2):
                 if nums1[i] == nums2[j]:
                     return nums1[i]
+        return -1
         '''
 
         #Brute 
@@ -35,5 +40,5 @@ class Solution:
             for j in range(n2):
                 if nums1[i] == nums2[j]:
                     ans = min(ans, nums1[i])
-        return ans
+        return -1 if ans == 10**18 else ans
         '''
