f055020d5a859c09fcd8abdd4b1494be4845bbfd
Shreyy <shreyw134@gmail.com>
2026-05-21 00:21:20 +0530

    Time: 51 ms (40.23%) | Memory: 36 MB (64.30%) - LeetSync

--- 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.py
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.py
@@ -5,11 +5,11 @@ class Solution:
         dic = defaultdict(int)
         for i in arr:
             dic[i] += 1
-        dic = dict(sorted(dic.items(), key=lambda item: item[1], reverse = True))
+        freq = sorted(dic.values(),reverse = True)
         ans = 0
         ss = 0
-        for i,j in dic.items():
-            ss += j
+        for i in freq:
+            ss += i
             ans += 1
             if ss >= half:
                 break
