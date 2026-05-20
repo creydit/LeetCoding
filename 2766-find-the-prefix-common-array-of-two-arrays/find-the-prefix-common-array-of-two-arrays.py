e05c30ff3771c894005a4175a1128f79eca89b01
Shreyy <shreyw134@gmail.com>
2026-05-20 11:19:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (73.05%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -1,16 +1,15 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
         n = len(A)
-        hashA = [0]*(n+1)
-        hashB = [0]*(n+1)
+        hash = [0]*(n+1)
         ans = [0]*n
         common = 0
         for i in range(n):
-            hashA[A[i]] = 1
-            if hashB[A[i]] == 1:
+            hash[A[i]] += 1
+            if hash[A[i]] >= 2:
                 common += 1
-            hashB[B[i]] = 1
-            if hashA[B[i]] == 1:
+            hash[B[i]] += 1
+            if hash[B[i]] >= 2:
                 common += 1
             ans[i] = common
         return ans
