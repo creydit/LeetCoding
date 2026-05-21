fdd0f24d20e53526d74e3ae8243d91bae8f62738
Shreyy <shreyw134@gmail.com>
2026-05-21 07:39:23 +0530

    Time: 8 ms (69.97%) | Memory: 21.8 MB (34.93%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -2,39 +2,18 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        dp = [[-1 for _ in range(m)] for __ in range(n)]
+        dp = [[0 for _ in range(m)] for __ in range(n)]
+        dp[0][0] = grid[0][0]
 
-        for i in range(n):
-            for j in range(m):
-                if i==0 and j==0:
-                    dp[i][j] = grid[i][j]
-                else:
-                    up = grid[i][j] 
-                    if j > 0:
-                        up += dp[i][j-1]
-                    else:
-                        up = 1e9
-                    
-                    left = grid[i][j]
-                    if i > 0:
-                        left += dp[i-1][j]
-                    else:
-                        left = 1e9
-                    dp[i][j] = min(up,left)
-                
-        '''
-        def solve(r, c):
-            if r == n-1 and c == m-1:
-                return grid[r][c]
-            
-            if r >= n or c >= m:
-                return 1e9
-            
-            if dp[r][c] != -1:
-                return dp[r][c]
-            
-            dp[r][c] = grid[r][c] + min(solve(r+1,c), solve(r,c+1))
-            return dp[r][c]
-        '''
-        ans = dp[n-1][m-1]
-        return ans 
+        for i in range(1,n):
+            dp[i][0] = grid[i][0] + dp[i-1][0]
+
+        for j in range(1,m):
+            dp[0][j] = grid[0][j] + dp[0][j-1]
+
+        for i in range(1,n):
+            for j in range(1, m):
+                dp[i][j] = grid[i][j]
+                dp[i][j] += min(dp[i-1][j], dp[i][j-1])
+
+        return dp[n-1][m-1]  
