9d3c3025c31c7dae049a277287a182346c4f0035
Shreyy <shreyw134@gmail.com>
2026-05-27 21:38:21 +0530

    Time: 190 ms (91.76%) | Memory: 21.6 MB (28.82%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
@@ -1,5 +1,24 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
+        #good solution 
+        n = len(word)
+        lastlow = defaultdict(int)
+        firstup = defaultdict(int)
+        for i in range(n):
+            if word[i].islower():
+                lastlow[word[i]] = i
+            else:
+                if word[i] not in firstup:
+                    firstup[word[i]] = i
+        ans = 0
+        for i in 'abcdefghijklmnopqrstuvwxyz':
+            if i in lastlow and i.upper() in firstup and lastlow[i] < firstup[i.upper()]:
+                ans += 1
+        return ans
+
+
+        #My Code T_T
+        '''
         ans = 0
         dic = defaultdict(int)
         dic1 = defaultdict(int)
@@ -14,3 +33,4 @@ class Solution:
                     ans += 1
                     dic[x] -= 1
         return ans
+        '''
