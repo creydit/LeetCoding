875433a791046fce32a7bad5a2e10c5070fd503e
Shreyy <shreyw134@gmail.com>
2026-05-29 11:09:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (21.61%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
@@ -1,16 +1,14 @@
 class Solution:
     def minElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        
         def ds(n):
             ss = 0
             while n > 0:
                 ss += n%10
                 n//=10
             return ss
-
-        arr = [0]*n
-        for i in range(n):
-            arr[i] = ds(nums[i])
         
-        return min(arr)
+        ans = 10**18
+        for i in nums:
+            ans = min(ans, ds(i))
+        
+        return ans
