a152987af38a15fb377af006a1da6d1c09f4841d
Shreyy <shreyw134@gmail.com>
2026-06-06 17:12:45 +0530

    Time: 2 ms (79.09%) | Memory: 19.5 MB (57.43%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.py
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.py
@@ -1,6 +1,17 @@
 class Solution:
     def leftRightDifference(self, nums: List[int]) -> List[int]:
+        # Better Beats code
+        totsum = sum(nums)
+        left = 0 
+        ans = []
+        for i in nums:
+            ans.append(abs(totsum - left - i))
+            left += i
+            totsum -= i
+        return ans
+
         #Optimal 2 pass O(N) solution
+        '''
         n = len(nums)
         ans = [0]*n
         runsum = 0
@@ -11,3 +22,4 @@ class Solution:
             right = totsum - runsum
             ans[i] = abs(left - right)
         return ans
+        '''
