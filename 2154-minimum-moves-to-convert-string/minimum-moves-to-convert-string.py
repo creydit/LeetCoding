90de7f00d1cb2ec2d494b7e0bb26f1894fc971a5
Shreyy <shreyw134@gmail.com>
2026-06-07 19:42:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (48.72%) - LeetSync

---
+++ 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumMoves(self, s: str) -> int:
+        n = len(s)
+        i = 0
+        ans = 0
+        while i < n:
+            if s[i] == 'X':
+                ans += 1
+                i += 3
+            else:
+                i += 1
+        return ans
