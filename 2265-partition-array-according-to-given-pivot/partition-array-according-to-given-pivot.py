db5118bd7ba18410e2ed37ee24103b33371a878b
Shreyy <shreyw134@gmail.com>
2026-06-08 23:59:07 +0530

    Time: 52 ms (20.17%) | Memory: 34 MB (10.42%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,6 +1,23 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        #Brute force
+        #Another 2 pointer approach
+        n = len(nums)
+        ans = [0]*n
+        li = 0
+        gi = n-1
+        for i in range(n):
+            if nums[i] < pivot:
+                ans[li] = nums[i]
+                li += 1
+        for j in range(n-1,-1,-1):
+            if nums[j] > pivot:
+                ans[gi] = nums[j]
+                gi -= 1
+        while li <= gi:
+            ans[li] = pivot
+            li += 1
+        return ans
+        #TC - O(n) and SC - O(n)
         n = len(nums)
         smalls = []
         equals = []
