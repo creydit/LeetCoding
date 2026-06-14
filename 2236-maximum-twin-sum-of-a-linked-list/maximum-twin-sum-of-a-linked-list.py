4a93f62148e0d1b9c034790a3db79e78b508fade
Shreyy <shreyw134@gmail.com>
2026-06-14 13:20:58 +0530

    Time: 70 ms (41.59%) | Memory: 38 MB (99.64%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
@@ -5,8 +5,26 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
+        #Optimal without extra space using Flyod Tortoise and HAre Algo with Reversal of first half
+        #TC - O(N) and SC - O(1)
+        fast = head
+        slow = head
+        prev = None
+        while fast and fast.next:
+            fast = fast.next.next
+            slow.next, prev, slow = prev, slow, slow.next
+            
+        ans = 0
+        while slow:
+            ans = max(ans, slow.val + prev.val)
+            slow = slow.next
+            prev = prev.next
+        
+        return ans
+
         #Brute Force using array space
         #TC - O(N + N/2) SC - O(N)
+        '''
         arr = []
         temp = head
         n = 0
@@ -18,3 +36,4 @@ class Solution:
         for i in range(n//2):
             ans = max(ans, arr[i] + arr[n-1-i])
         return ans 
+        '''
