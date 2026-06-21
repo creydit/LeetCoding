48a832c3060a82e932661be2935a2309b6f8e6f6
Shreyy <shreyw134@gmail.com>
2026-06-21 13:49:23 +0530

    Time: 112 ms (85.39%) | Memory: 20.8 MB (56.18%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -1,32 +1,17 @@
-class Solution:
-    def maxIceCream(self, costs: List[int], coins: int) -> int:
-        #Counting Sort 
-        mx = max(costs)
-        freq = [0]*(mx+1)
-        for i in costs:
-            freq[i] += 1
-        ans = 0
-        for i in range(1,mx+1):
-            if freq[i] > 0:
-                take = coins//i
-                if freq[i] <= take:
-                    ans += freq[i]
-                    coins -= (freq[i]*i)
-                else:
-                    ans += take
-                    coins -= (take*i)
-        return ans
-                
-        #Normal way
-        '''
-        n = len(costs)
+class Solution(object):
+    def maxIceCream(self, costs, coins):
+        """
+        :type costs: List[int]
+        :type coins: int
+        :rtype: int
+        """
         costs.sort()
         ans = 0
-        for i in range(n):
-            if coins >= costs[i]:
-                ans += 1
-                coins -= costs[i]
-            else:
+        for i in costs:
+            if i > coins:
                 break
+            else:
+                ans += 1
+                coins -= i
         return ans
-        '''
+        
