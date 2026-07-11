b5b37a5a9213d8212809a792f2ebb241edb205f3
Shreyy <shreyw134@gmail.com>
2026-07-11 20:54:03 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (46.93%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.py
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.py
@@ -6,6 +6,33 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
+        #TC - O(log^2N) and SC - O(H)
+        if root is None:
+            return 0
+        def leftH(node):
+            hh = 0
+            while node:
+                hh+=1
+                node = node.left
+            return hh
+        
+        def rightH(node):
+            hh = 0
+            while node:
+                hh += 1
+                node = node.right
+            return hh
+        
+        lh = leftH(root)
+        rh = rightH(root)
+
+        if lh == rh: return 2**lh - 1
+
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+
+
+        #TC -O(N) and SC - O(height of tree, auxillary)
+        '''
         ans = 0
         def dfs(node):
             nonlocal ans
@@ -16,3 +43,4 @@ class Solution:
             dfs(node.right)
         dfs(root)
         return ans 
+        '''
