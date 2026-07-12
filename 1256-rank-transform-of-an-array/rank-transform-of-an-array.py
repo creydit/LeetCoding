b9af58e555922f96d800aa681a04bb7cd0cf9791
Shreyy <shreyw134@gmail.com>
2026-07-12 19:54:11 +0530

    Time: 142 ms (5.22%) | Memory: 38 MB (14.35%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -1,7 +1,28 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        n = len(arr)
+        if n==0:
+            return []
+        nums = []
+        for i in range(n):
+            nums.append((arr[i],i))
+        nums.sort()
+        idx = nums[0][1]
+        arr[idx] = 1
+        rank = 1
+        for i in range(1,n):
+            if nums[i][0] == nums[i-1][0]:
+                idx = nums[i][1]
+                arr[idx] = rank
+            else:
+                rank += 1
+                idx = nums[i][1]
+                arr[idx] = rank 
+        return arr
+
         #Brute force
-        #TC - O(N) and SC - O(N)
+        #TC - O(NlogN) and SC - O(N)
+        '''
         n = len(arr)
         dic = {}
         nums = sorted(arr)
@@ -13,3 +34,4 @@ class Solution:
         for i in range(n):
             arr[i] = dic[arr[i]]
         return arr
+        '''
