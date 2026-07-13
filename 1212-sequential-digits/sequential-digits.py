dffb8318614a61248cf4d727cd2b8df3731feaa7
Shreyy <shreyw134@gmail.com>
2026-07-13 12:13:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (88.24%) - LeetSync

--- 1212-sequential-digits/sequential-digits.py
+++ 1212-sequential-digits/sequential-digits.py
@@ -1,6 +1,19 @@
 class Solution:
-    def sequentialDigits(self, low: int, high: int) -> List[int]:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:\
+        #Using BFS
+        q = [i for i in range(1,10)]
+        for i in q:
+            d = i%10
+            if d < 9:
+                q.append(i*10 + d + 1)
+        ans = []
+        for i in q:
+            if low<=i<=high:
+                ans.append(i)
+        return ans
+        
         #TC - O(nlogn, n is max num of increasing digits) and SC - O(1)
+        '''
         ans = []
         s = '123456789'
         l = 0
@@ -13,3 +26,4 @@ class Solution:
                     ans.append(num)
         ans.sort()
         return ans
+        '''
