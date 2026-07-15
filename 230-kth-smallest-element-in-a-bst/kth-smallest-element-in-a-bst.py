25bc92f7bfdfc33f1438b7af1a522022bdc8a6ed
Shreyy <shreyw134@gmail.com>
2026-07-15 19:05:32 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (39.36%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,8 +6,26 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        #Optimal using property of BST thatr inorder of BST is always sorted
+        cnt = 0
+        ans = None
+        def dfs(node):
+            nonlocal cnt
+            nonlocal ans
+            if node is None or ans is not None:
+                return 
+            dfs(node.left)
+            cnt += 1
+            if cnt == k:
+                ans = node.val
+                return
+            dfs(node.right)
+        dfs(root)
+        return ans
+
         #Brute - sorting value in arr and finding
         #TC - O(NlogN) and SC - O(1)
+        '''
         arr = []
         def dfs(node):
             if node is None:
@@ -18,3 +36,4 @@ class Solution:
         dfs(root)
         arr.sort()
         return arr[k-1]
+        '''
