0d5459c9cd8f241dc52317fe8242202df93744b1
Shreyy <shreyw134@gmail.com>
2026-07-24 18:06:02 +0530

    Time: 11 ms (44.79%) | Memory: 21.2 MB (10.90%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,7 +1,25 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        #Optimal using 2 pointers
+        maxL = height[0]
+        maxR = height[-1]
+        l = 0
+        r = len(height)-1
+        ans = 0
+        while l < r:
+            if maxL <= maxR:
+                l += 1
+                maxL = max(maxL, height[l])
+                ans += maxL - height[l]
+            else:
+                r -= 1
+                maxR = max(maxR, height[r])
+                ans += maxR - height[r]
+        return ans
+
         #using preffix max and suffix max 
         #TC - O(N+N+N) and SC- O(N+N)
+        '''
         n = len(height)
         prefmax = [0]*n
         prefmax[0] = height[0]
@@ -16,3 +34,4 @@ class Solution:
             if height[i] < prefmax[i] and height[i] < suffmax[i]:
                 ans += min(prefmax[i],suffmax[i]) - height[i]
         return ans 
+        '''
