614e792d24eec244fef684dfca647dc9477267e8
Shreyy <shreyw134@gmail.com>
2026-09-04 21:58:40 +0530

    Time: 19 ms (11.71%) | Memory: 19.4 MB (94.90%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/longest-valid-parentheses.py
@@ -1,5 +1,36 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
+        #TC - O(2n) and SC - O(1)
+        n = len(s)
+        ans = 0
+        left = 0 
+        right = 0
+        for i in s:
+            if i=='(':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                ans = max(ans, left + right)
+            if right > left:
+                left = 0
+                right = 0
+        left = 0
+        right = 0
+        for i in range(n-1,-1,-1):
+            if s[i] == '(':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                ans = max(ans, left+right)
+            if right < left:
+                left = 0
+                right = 0
+        return ans
+
+        #TC -O(n) and SC - O(n)
+        '''
         n = len(s)
         ans = 0
         st = [-1]
@@ -13,3 +44,4 @@ class Solution:
                 else:
                     ans = max(ans, i - st[-1])
         return ans
+        '''
